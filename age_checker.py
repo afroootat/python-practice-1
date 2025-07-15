3b6442fe40ed6c31b346f5d40d97b5aef5ff1325
Olaleye Tofarati <167630546+afroootat@users.noreply.github.com>
2025-07-15 18:42:18 +0100

    Update age_checker.py

--- age_checker.py
+++ age_checker.py
@@ -1,6 +1,9 @@
+# Ask the user to enter their age
 age = int(input("Enter your age: "))
 
+# Check if the person is 18 or older
 if age >= 18:
     print("You are an adult!")
 else:
+    # If they're younger than 18
     print("You are not an adult!")
