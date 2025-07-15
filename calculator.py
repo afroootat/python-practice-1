dfd9219a9ad9af84d3ea5c463cf402c17dcc26c9
Olaleye Tofarati <167630546+afroootat@users.noreply.github.com>
2025-07-15 18:41:20 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,7 +1,13 @@
+# Ask the user to enter the first number
 num1 = float(input("Enter a number: "))
+
+# Ask the user to enter the second number
 num2 = float(input("Enter another number: "))
+
+# Ask the user to choose an operation
 operation = input("Enter an operation(+, -, *, /): ")
 
+# Check what operation was chosen and do the calculation
 if operation == "+":
     print(f"Result: {num1 + num2}")
 elif operation == "-":
@@ -11,4 +17,5 @@ elif operation == "*":
 elif operation == "/":
     print(f"Result: {num1 / num2}")
 else:
+    # If the user types something other than +, -, *, /
     print("Enter a valid operation!")
